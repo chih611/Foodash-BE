678955c691c5043ae17d4a9af929d39c28d13022
chih611 <chihao.thai@gmail.com>
2024-09-03 17:02:08 +1000

    intergate BE

--- src/controllers/itemControllers.js
+++ src/controllers/itemControllers.js
@@ -1,9 +1,10 @@
 const connection = require('../config/database');
-
+console.log(connection);
 const getItems = (req, res) => {
     connection.query('SELECT * FROM ITEMS', function (err, rows) {
         res.status(200).json(rows);
     });
+    // return res.render('index');
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
-
+const cors = require('cors');
+const path = require('path');
 const express = require('express');
 const app = express();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 
 const port = process.env.SERVER_PORT;
-
 configViewEngine(app);
 
+app.use(cors());
 app.use('/v1', webRoutes);
+// config static file
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)

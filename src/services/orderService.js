bcf32cf11cd3d4af637fb130c7d2afe6ba162237
chih611 <chihao.thai@gmail.com>
2024-10-07 22:09:58 +1100

    [FOO-193] [FE] [AV] Order features

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,7 +1,9 @@
 const connection = require("../config/database");
 
 const getAllorders = async () => {
-  const [rows] = await connection.query(`SELECT * FROM foodash.ORDER_VIEW;`);
+  const [rows] = await connection.query(
+    "SELECT ID, `Full Name`, Duedate, `Create Date`, Total, Status FROM foodash.ORDER_VIEW;"
+  );
   return { rows };
 };
 

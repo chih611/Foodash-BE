80b00d37f4a0f5147c113325f303fbd08528e56d
chih611 <chihao.thai@gmail.com>
2024-10-31 17:44:30 +1100

    update pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "ec2.foodash.org",
+      script: "./server.js",
+      env: {
+        DB_USER: "admin",
+        DB_PASSWORD: "capstone",
+        DB_NAME: "foodash",
+        DB_HOST:
+          "db-instance-foodash.ctcw2oo2m3zo.ap-southeast-2.rds.amazonaws.com",
+      },
+    },
+  ],
+};
--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ app.set("views", path.join(__dirname, "src", "views"));
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-app.use(cors({ origin: 'http://ec2.foodash.org' }));
+app.use(cors());
+// app.use(cors({ origin: 'http://ec2.foodash.org' }));
 configViewEngine(app);
 
 app.use("/", webRoutes);
